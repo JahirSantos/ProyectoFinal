9fe0cd15079ecbbcde7f2b7f60101bd1d12d4a46
Jahir Santos Urbina <122399177+JahirSantos@users.noreply.github.com>
2023-08-27 14:20:05 -0500

    Maquetacion Final
    listo

--- index_script.js
+++ index_script.js
@@ -9,3 +9,14 @@ listItems.forEach((item) =>{
         else item.classList.add("active");
     });
 });
+const toggleSidebar = document.querySelector(".toggle-sidebar");
+const logo = document.querySelector(".logo-box");
+const sidebar = document.querySelector(".sidebar");
+
+toggleSidebar.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
+
+logo.addEventListener("click", () => {
+  sidebar.classList.toggle("close");
+});
